50ae321cd0632f63c5da58ff1379c78425c7cd4e
Talal Zafar <talalzafar84@gmail.com>
2025-03-26 16:33:39 +0500

    >

--- routers/emailRoutes.js
+++ routers/emailRoutes.js
@@ -5,10 +5,11 @@ const {
   TrackEmail,
 } = require("../controllers/emailGeneration");
 
+const authMiddleware = require("../middlewares/authMiddleware");
 const router = express.Router();
 
-router.post("/generate",authMiddleware, GenerateEmail);
-router.post("/makeChanges",authMiddleware, MakeChangesToEmail);
+router.post("/generate", authMiddleware, GenerateEmail);
+router.post("/makeChanges", authMiddleware, MakeChangesToEmail);
 router.get("/emailTracker", TrackEmail);
 
 module.exports = router;
